ec5171b81fd84e0881c88938a5d09d29e7de1297
mohsin <mkrizvi98@gmail.com>
2017-10-29 13:54:48 -0400

    Jumping enhansed with multiple jump heights. Check out for bug where player moves in a direction uncontrollably

---
+++ Assets/_Scripts/Jumping.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jumping : MonoBehaviour {
+
+	public float jumpSpeed = 4.5f;
+	public float higherJumpTime = 0.07f;
+	public float higherJumpSpeed = 0.7f;
+	public int maxHigherJumps = 1;
+	[HideInInspector]
+	public bool isGrounded = false;
+	public float jumpBuffer = 0.15f;
+	public float fallingGravityScale = 1.1f;
+
+	private Rigidbody2D rb2d;
+	private float buffer_counter = 0;
+	private float realGravity;
+	private bool isBufferedJump = false;
+	private int higherJumpCounter = 0;
+
+	void Start()
+	{
+		rb2d = GetComponentInParent<Rigidbody2D> ();
+		realGravity = rb2d.gravityScale;
+	}
+
+	void OnCollisionStay2D(Collision2D coll) {
+		if (coll.gameObject.tag == "Ground" || coll.gameObject.tag == "Blue" || coll.gameObject.tag == "Red")
+			isGrounded = true;
+	}
+
+	void OnCollisionExit2D(Collision2D coll)
+	{
+		if (coll.gameObject.tag == "Ground" || coll.gameObject.tag == "Blue" || coll.gameObject.tag == "Red")
+			isGrounded = false;
+	}
+
+	private void FixedUpdate()
+	{
+		/* Check for buffered jumping. */
+		if (isBufferedJump) {
+			buffer_counter += Time.deltaTime;
+			Debug.Log (buffer_counter);
+			if (buffer_counter > jumpBuffer)
+				isBufferedJump = false;
+		}
+
+		/* Check for jumping */
+		if (((Input.GetButtonDown ("Jump")) || isBufferedJump) && isGrounded) {
+			isGrounded = false;
+			isBufferedJump = false;
+			rb2d.velocity = new Vector2(rb2d.velocity.x, jumpSpeed);
+			StartCoroutine (WaitForAddForce ());
+		}
+
+		if (Input.GetButtonDown ("Jump") && !isGrounded) {
+			isBufferedJump = true;
+			buffer_counter = 0;
+		}
+
+		/* Increase gravity slightly when falling. */
+		if (rb2d.velocity.y < 0)
+			rb2d.gravityScale = fallingGravityScale;
+		else
+			rb2d.gravityScale = realGravity;
+	}
+
+	private IEnumerator WaitForAddForce()
+	{
+		while (higherJumpCounter < maxHigherJumps) {
+			yield return new WaitForSeconds (higherJumpTime);
+			if (Input.GetButton ("Jump")) {
+				rb2d.velocity += new Vector2 (0, higherJumpSpeed);
+				higherJumpCounter++;
+			} else {
+				break;
+			}
+		}
+		higherJumpCounter = 0;
+	}
+}
--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -5,63 +5,30 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
 
 	public float speed = 5.0f;
-	public float jumpSpeed = 5.0f;
-	public float colliderHeight = 0.30f;
 	public Vector2 moveDirection = Vector2.zero;
-	public Vector2 jumpDirection = Vector2.zero;
-	[HideInInspector]
-	public bool isGrounded = false;
-	public float jumpBuffer = 0.15f;
 
+	private float moveDir = 0.0f;
 	private Rigidbody2D rb2d;
-	private CapsuleCollider2D cc2d;
-	private float buffer_counter = 0;
-	private bool isBufferedJump = false;
 
 	void Start()
 	{
 		rb2d = GetComponent<Rigidbody2D> ();
 	}
 
-	void OnCollisionStay2D(Collision2D coll) {
-		if (coll.gameObject.tag == "Ground" || coll.gameObject.tag == "Blue" || coll.gameObject.tag == "Red")
-			isGrounded = true;
-	}
-
-	void OnCollisionExit2D(Collision2D coll)
-	{
-		if (coll.gameObject.tag == "Ground" || coll.gameObject.tag == "Blue" || coll.gameObject.tag == "Red")
-			isGrounded = false;
-	}
-
 	void FixedUpdate()
 	{
+//		moveDir = Input.GetAxis ("Horizontal");
+//		/* Check for horizontal movement */
+//		if (moveDir != 0) {
+//			rb2d.velocity = new Vector2 (speed * moveDir, rb2d.velocity.y);
+//		} else {
+//			rb2d.velocity = new Vector2 (0, rb2d.velocity.y);
+//		}
 
-		/* Check for horizontal movement */
-		if (Input.GetAxis("Horizontal") != 0) {
+		if (Input.GetAxis("Horizontal") != 0)
 			moveDirection = new Vector2(Input.GetAxisRaw("Horizontal"), 0);
-		}
 
-		if (isBufferedJump) {
-			buffer_counter += Time.deltaTime;
-			Debug.Log (buffer_counter);
-			if (buffer_counter > jumpBuffer)
-				isBufferedJump = false;
-		}
-
-		/* Check for jumping */
-		if (((Input.GetButtonDown ("Jump")) || isBufferedJump) && isGrounded) {
-			isGrounded = false;
-			isBufferedJump = false;
-			rb2d.velocity = new Vector3(rb2d.velocity.x, jumpSpeed, rb2d.velocity.y);
-		}
-
-		if (Input.GetButtonDown ("Jump") && !isGrounded) {
-			isBufferedJump = true;
-			buffer_counter = 0;
-		}
-		/* Execute movement */
+		/* Execute movement. */
 		transform.Translate (moveDirection.normalized * speed * Time.deltaTime);
 	}
-
 }
