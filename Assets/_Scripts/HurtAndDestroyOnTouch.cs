4fe39bb5a4a6e11072a48dc041fde3904ecd70ef
mohsin <mkrizvi98@gmail.com>
2017-11-16 11:52:49 -0500

    added downward plate throwers

---
+++ Assets/_Scripts/HurtAndDestroyOnTouch.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HurtAndDestroyOnTouch : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		PlayerDie pd;
+
+		if (other.tag == "Player") {
+			pd = other.GetComponent<PlayerDie> ();
+			if (pd != null)
+				pd.Hurt ();
+		}
+
+		if (other.tag == "Player"   ||
+			other.tag == "Ground"   ||
+			other.tag == "RedHor"   ||
+			other.tag == "BlueHor"  ||
+			other.tag == "RedVert"  ||
+			other.tag == "BlueVert" ||
+			other.tag == "Red")
+			Destroy (gameObject);
+	}
+}
---
+++ Assets/_Scripts/ProjectileSpawner.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileSpawner : MonoBehaviour {
+
+	public GameObject spawnedObject;
+	public float timeBetweenSpawns = 1.0f;
+	public Vector3 spawnOffset;
+
+	void Start()
+	{
+		StartCoroutine (TimedSpawner());
+	}
+
+	IEnumerator TimedSpawner()
+	{
+		while (gameObject.activeSelf) {
+			yield return new WaitForSeconds (timeBetweenSpawns);
+			Spawn ();
+		}
+	}
+
+	void Spawn()
+	{
+		GameObject spawned = Instantiate (spawnedObject);
+		spawned.transform.position = transform.position + spawnOffset;
+	}
+}
