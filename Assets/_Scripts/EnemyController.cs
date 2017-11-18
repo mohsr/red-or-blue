000c2cb4a37aef264e11d0a5ef1a59ce7f0ed7a7
tommyzty <tianyu.zhu@tufts.edu>
2017-11-18 01:38:09 -0500

    enemy now goes on patrol, and increase speed to follow the player once the player is in line of sight

--- Assets/_Scripts/EnemyController.cs
+++ Assets/_Scripts/EnemyController.cs
@@ -10,7 +10,7 @@ public class EnemyController : MonoBehaviour {
     public float fallSpeedMultiplier = 1.1f;
     public LayerMask enemyMask;
 
-    public Vector3 visionOffset = new Vector2(0.0f, 0.0f);
+    public Vector2 visionOffset = new Vector2(0.0f, 0.0f);
 
     private bool falling;
     private Animator anim;
@@ -25,8 +25,6 @@ public class EnemyController : MonoBehaviour {
     float directionChangeBuffer = 0.5f;
     bool isChangingDirection = false;
     float directionBufferCounter = 0;
-
-    public bool moveOnSight = true;
 
     private void Start()
     {
@@ -50,21 +48,15 @@ public class EnemyController : MonoBehaviour {
         {
             Fall();
         }
-        else if (!embarrassed)
-        {
-            if (moveOnSight)
-            {
-                MoveToPlayer();
-            }
-            else
-            {
-                AutoTurn();
-            }
-        } else
+        else if (embarrassed)
         {
             Vector2 myVelocity = myBody.velocity;
             myVelocity.x = 0;
             myBody.velocity = myVelocity;
+        }
+        else
+        {
+            MoveToPlayer();
         }
     }
 
@@ -82,6 +74,7 @@ public class EnemyController : MonoBehaviour {
         stomped = false;
 
         Physics2D.IgnoreCollision(gameObject.GetComponent<Collider2D>(), GameObject.FindGameObjectWithTag("Ground").GetComponent<Collider2D>());
+        Physics2D.IgnoreCollision(gameObject.GetComponent<Collider2D>(), GameObject.FindGameObjectWithTag("Player").GetComponent<Collider2D>());
 
         falling = true;
         Vector2 newVelocity = myBody.velocity;
@@ -98,48 +91,87 @@ public class EnemyController : MonoBehaviour {
 
     private void MoveToPlayer()
     {
-        int i;
         RaycastHit2D point;
 
         /* Cast a line to the left, then to the right. */
-        for (i = -1; i < 2; i += 2)
+
+        /* Draw a line. */
+        Vector2 lineCastPos = myTrans.position.toVector2();
+        point = Physics2D.Linecast(lineCastPos + visionOffset,
+            lineCastPos - myTrans.right.toVector2() * viewDistance, ~(1 << LayerMask.NameToLayer("Enemy")));
+        /* Draw the line in scene viewer. */
+        Debug.DrawLine(lineCastPos + visionOffset,
+            lineCastPos - myTrans.right.toVector2() * viewDistance, Color.red);
+        
+        /* If player found, move. */
+        if (point.collider != null && point.collider.tag == "Player")
         {
-            /* Draw a line. */
-            point = Physics2D.Linecast(transform.position + visionOffset,
-                new Vector2(transform.position.x + visionOffset.x + (i * viewDistance),
-                    transform.position.y + visionOffset.y),
-                ~(1 << LayerMask.NameToLayer("Enemy")));
-
-            /* Draw the line in scene viewer. */
-            Debug.DrawLine(transform.position + visionOffset, new Vector2(transform.position.x + visionOffset.x + (i * viewDistance),
-                transform.position.y + visionOffset.y));
-
-            /* If player found, move. */
-            if (point.collider == null)
+            /*Vector2 myVelocity = myBody.velocity;
+            myVelocity.x = 0;
+            myBody.velocity = myVelocity;*/
+            transform.position = Vector2.MoveTowards(transform.position,
+                point.collider.transform.position,
+                speed * Time.deltaTime);
+                    
+            if (!anim.GetBool("Walking"))
             {
-                if (!anim.GetBool("Idle"))
-                {
-                    anim.SetBool("Idle", true);
-                    anim.SetBool("Walking", false);
-                }
-                continue;
+                anim.SetBool("Idle", false);
+                anim.SetBool("Walking", true);
             }
-            if (point.collider.tag == "Player")
-            {
-                transform.position = Vector2.MoveTowards(transform.position,
-                    point.collider.transform.position,
-                    speed * Time.deltaTime);
-                if (transform.localScale.x != -i)
-                    transform.localScale = new Vector3(-i, transform.localScale.y, transform.localScale.z);
-                if (!anim.GetBool("Walking"))
-                {
-                    anim.SetBool("Idle", false);
-                    anim.SetBool("Walking", true);
-                }
-                return;
-            }
+            return;
+        }
+        else
+        {
+            AutoTurn();
         }
     }
+
+
+
+    //private void MoveToPlayer()
+    //{
+    //    int i;
+    //    RaycastHit2D point;
+
+    //    /* Cast a line to the left, then to the right. */
+    //    for (i = -1; i< 2; i += 2)
+    //    {
+    //        /* Draw a line. */
+    //        point = Physics2D.Linecast(transform.position + visionOffset,
+    //            new Vector2(transform.position.x + visionOffset.x + (i* viewDistance),
+    //                transform.position.y + visionOffset.y),
+    //            ~(1 << LayerMask.NameToLayer("Enemy")));
+
+    //        /* Draw the line in scene viewer. */
+    //        Debug.DrawLine(transform.position + visionOffset, new Vector2(transform.position.x + visionOffset.x + (i* viewDistance),
+    //            transform.position.y + visionOffset.y));
+
+    //        /* If player found, move. */
+    //        if (point.collider == null)
+    //        {
+    //            if (!anim.GetBool("Idle"))
+    //            {
+    //                anim.SetBool("Idle", true);
+    //                anim.SetBool("Walking", false);
+    //            }
+    //            continue;
+    //        }
+    //        if (point.collider.tag == "Player")
+    //        {
+    //            transform.position = Vector2.MoveTowards(transform.position,
+    //                point.collider.transform.position,
+    //                speed* Time.deltaTime);
+    //            if (transform.localScale.x != -i)
+    //                transform.localScale = new Vector3(-i, transform.localScale.y, transform.localScale.z);
+    //            if (!anim.GetBool("Walking"))
+    //            {
+    //                anim.SetBool("Idle", false);
+    //                anim.SetBool("Walking", true);
+    //            }
+    //            return;
+    //        }
+    //    }
+    //}
 
     private void AutoTurn()
     {
