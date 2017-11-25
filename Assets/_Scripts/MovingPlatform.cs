125ea55ee757e4de6d0b1fe1e807a142265414d7
mohsin <mkrizvi98@gmail.com>
2017-11-25 16:51:27 -0500

    finalized moving platforms

--- Assets/_Scripts/MovingPlatform.cs
+++ Assets/_Scripts/MovingPlatform.cs
@@ -6,15 +6,14 @@ public class MovingPlatform : MonoBehaviour {
 
 	public Vector3[] movementPath;
 	public float speed = 2.0f;
-	public float waitAtEnds = 1.0f;
-	public float waitTime = 2.0f;
+	public float waitAtEndsTime = 2.0f;
 	public bool oneWay = false;
 
 	private Rigidbody2D rb2d;
 
 	private Vector3 initLoc;
 	private Vector3 currLoc;
-	private int currSpot;
+	private int currSpot = 0;
 	private int nextSpot;
 	private bool ascending;
 	private bool fin = false;
@@ -25,7 +24,7 @@ public class MovingPlatform : MonoBehaviour {
 		rb2d = GetComponent<Rigidbody2D> ();
 		ascending = true;
 
-		movementPath [0] = transform.position;
+		movementPath [0] = new Vector3 (0, 0, 0);
 		initLoc = transform.position;
 	}
 
@@ -89,7 +88,7 @@ public class MovingPlatform : MonoBehaviour {
 	IEnumerator WaitAtEnd()
 	{
 		moving = false;
-		yield return new WaitForSeconds (waitTime);
+		yield return new WaitForSeconds (waitAtEndsTime);
 		moving = true;
 	}
 }
