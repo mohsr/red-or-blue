e211bca10f3663f6cc516ee49c22663e3cae842d
Mohsin Rizvi <mkrizvi98@gmail.com>
2017-12-12 17:45:20 -0500

    added main menu to cutscene to bathroom

---
+++ Assets/_Scripts/WaitThenLoadScene.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WaitThenLoadScene : MonoBehaviour {
+
+	public float waitTime = 1.8f;
+	public string levelName = "Bathroom";
+
+	void Start()
+	{
+		StartCoroutine (WaitThenLoad ());
+	}
+
+	public IEnumerator WaitThenLoad()
+	{
+		yield return new WaitForSeconds(waitTime);
+		SceneManager.LoadScene (levelName);
+	}
+}
