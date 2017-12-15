7e1320698b27676ad798653057178c1629f73fef
Daniel Cetlin <daniel.cetlin@tufts.edu>
2017-12-15 10:29:39 -0500

    Finishing up bathroom intro level

---
+++ Assets/Transparent.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+public class Transparent : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		// store a reference to the SpriteRenderer on the current GameObject
+		SpriteRenderer [] invisibles = gameObject.transform.GetComponentsInChildren<SpriteRenderer>();
+		foreach (SpriteRenderer s in invisibles) 
+		{
+			Color col = s.color;
+			col.a = 0.5f;
+			s.color = col;
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
