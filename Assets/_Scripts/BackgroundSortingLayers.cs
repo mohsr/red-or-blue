7b0a2c34a9c4323ad7fe1b901470b57216b405d8
ctang <tommy9695@gmail.com>
2017-12-19 15:27:14 -0500

    don't know what's happening

---
+++ Assets/_Scripts/BackgroundSortingLayers.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundSortingLayers : MonoBehaviour {
+
+	void Start()
+	{
+		for (int i = 0; i < transform.childCount; i++) {
+			transform.GetChild (i).GetComponent<MeshRenderer> ().sortingLayerName = "Background";
+			for (int j = 0; j < transform.GetChild (i).transform.childCount; j++) {
+				transform.GetChild (i).GetChild (j).GetComponent<MeshRenderer> ().sortingLayerName = "Background";
+			}
+		}
+	}
+}
