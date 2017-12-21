bb6e863b7d95920bd4562f290344aeafdc881828
ctang <tommy9695@gmail.com>
2017-12-20 21:18:07 -0500

    fixed bug with build. changed all build settings. SOOO MUCH BETTER

---
+++ Assets/_Scripts/SetFrameRate.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetFrameRate : MonoBehaviour {
+
+	void Start () {
+		Application.targetFrameRate = 60;
+	}
+}
